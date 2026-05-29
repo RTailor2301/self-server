3eb639daa80b11c0f78352936779f33b1a98e9c5
RTailor2301 <roshan.tailor3456@gmail.com>
2026-05-29 13:52:59 -0400

    db: aded users table

--- db/init/0001_schema.sql
+++ db/init/0001_schema.sql
@@ -3,3 +3,11 @@
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Users
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(128) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
