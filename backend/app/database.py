3d65013fdb42262c82098c7d552808c14d3d7dbc
RTailor2301 <roshan.tailor3456@gmail.com>
2026-06-04 12:37:49 -0400

    db: created get and check connections

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from collections.abc import Generator
 from contextlib import contextmanager
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, inspect
 from sqlalchemy import Connection, Engine
 
 from app.config import DATABASE_URL
@@ -10,3 +10,27 @@ engine: Engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
+
+'''
+Use with engine.connect() as conn for automatic closure
+'''
+
+@contextmanager
+def get_connection() -> Generator[Connection, None, None]: # [Yield Type, Send Type, Return Type]
+    '''
+    Open connection and yield for duration of operation
+    '''
+    with engine.connect() as conn:
+        with conn.begin():
+            yield conn
+
+def check_connection() -> None:
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+
+if __name__ == "__main__":
+    check_connection()
+    inspector = inspect(engine)
+    tables: list[str] = inspector.get_table_names()
+    print("connected:",DATABASE_URL.split("@")[-1])
+    print("tables:",tables)
